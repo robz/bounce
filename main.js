e6c2debcf39e81cef06644ef51dad4beabc83f21
robz <rjnevels@utexas.edu>
2014-03-03 12:47:10 -0600

    added key controls for paddle, added floor

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
             mass: 5
         }),
         
-        box = makeBox({
+        paddle = makeBox({
             receiveShadow: true,
             dimension: {
                 width: 150,
@@ -28,15 +28,48 @@
                 y: -50,
                 z: 0
             }
+        }),
+
+        floor = makeBox({
+            receiveShadow: true,
+            dimension: {
+                width: 500,
+                height: 3,
+                depth: 500
+            },
+            position: {
+                x: 0,
+                y: -100,
+                z: 0
+            }
         });
-    
+
     ball.setCollisionCallback(function () {
         document.getElementById('bounce_sound').play();
     });
     
-    simulator.addEntity(ball);
-    simulator.addEntity(box);
-    simulator.setEntityInteractionProperties(ball, box, 0.1, 0.99);
+    simulator.addEntities([ball, paddle, floor]);
+    simulator.setEntityInteractionProperties(ball, paddle, 0.1, 0.99);
+    simulator.setEntityInteractionProperties(ball, floor, 0.1, 0.99);
+
+    document.addEventListener("keydown", function keyDown(e) {
+        var key = String.fromCharCode(e.which);
+
+        switch (key) {
+            case "W":
+                paddle.translate(0, 0, -20);
+                break;
+            case "A":
+                paddle.translate(-20, 0, 0);
+                break;
+            case "S":
+                paddle.translate(0, 0, 20);
+                break;
+            case "D":
+                paddle.translate(20, 0, 0);
+                break;
+        }
+    }, false);
     
     simulator.start();
 }());
